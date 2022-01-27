43a2a89ecac4312b093d8cd4aecb2569345a8582
mikelu2000 <mikeylu@wustl.edu>
2022-01-27 12:16:05 -0600

    Average

---
+++ src/Average.java
@@ -0,0 +1,15 @@
+import support.cse131.ArgsProcessor;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int n1 = ap.nextInt("The first of two integers to be averaged?");
+		int n2 = ap.nextInt("The second of two integers to be averaged?");
+		
+		double average = (n1 + n2)/2.0;
+	 System.out.println("Average of " + n1 + " and " + n2 + " is "+ average + ".");
+	}
+
+}
